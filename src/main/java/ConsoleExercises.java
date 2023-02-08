cb4cbbd9af90d0d6f5709f8df8dc2294ed1b6984
Alex Schmerber <alex.g.schmerber@gmail.com>
2023-02-08 11:17:02 -0600

    post ConsoleExercises

--- src/main/java/ConsoleExercises.java
+++ src/main/java/ConsoleExercises.java
@@ -3,8 +3,11 @@ public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
         System.out.printf("The value of pi is approximately: %.2f\n", pi);
+//        System.out.format("The value of pi is approximately: %.2f\n", pi);
 
         Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner("4ouch5");
+//        scanner.useDelimiter('ouch')
 
         System.out.println("Enter an integer: ");
         int userInteger = scanner.nextInt();
@@ -21,15 +24,20 @@ public class ConsoleExercises {
         System.out.println("Enter a sentence: ");
         String userSentence = scanner.nextLine();
         System.out.printf("You entered: --> %s\n", userSentence);
-        scanner.nextLine();
 
-        System.out.println("Enter an the length of a rectangle: ");
-        int userWidth = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("Enter an the width of a rectangle: ");
-        int userLength = scanner.nextInt();
-        int perimeter = userWidth * 2 + userLength * 2;
-        System.out.printf("The perimeter of the rectangle is: --> %d\n", perimeter);
-        scanner.nextLine();
+        System.out.println("Enter an the length of a rectangular prism: ");
+        String userWidth = scanner.nextLine();
+        System.out.println("Enter an the width of a rectangular prism: ");
+        String userLength = scanner.nextLine();
+        System.out.println("Enter an the height of a rectangular prism: ");
+        String userHeight = scanner.nextLine();
+        int perimeter = Integer.parseInt(userWidth) * 2 + Integer.parseInt(userLength) * 2;
+        int area = Integer.parseInt(userWidth) * Integer.parseInt(userLength);
+        int volume = Integer.parseInt(userWidth) * Integer.parseInt(userLength) * Integer.parseInt(userHeight);
+        System.out.printf("""
+                The perimeter is %d
+                The area is %d
+                The volume is %d%n""", perimeter, area, volume);
+        scanner.close();
     }
 }
