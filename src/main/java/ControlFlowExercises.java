9d6ba84d43fc6202f5318548a000b222aca8c3c6
Alex Schmerber <alex.g.schmerber@gmail.com>
2023-02-08 14:22:24 -0600

    post ControlFlowExercises

--- src/main/java/ControlFlowExercises.java
+++ src/main/java/ControlFlowExercises.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 //        int i = 5;
 //        while(i <= 15){
 //            System.out.println(i);
@@ -14,19 +16,65 @@ public class ControlFlowExercises {
 //            i--;
 //        } while (i >= -10);
 
-        int i = 1;
-        while(i <= 100){
-            if(i % 3 == 0 && i % 5 == 0){
-                System.out.println("FizzBuzz");
-            } else if(i % 3 == 0){
-                System.out.println("Fizz");
-            }else if(i % 5 == 0){
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
+//        int i = 1;
+//        while(i <= 100){
+//            if(i % 3 == 0 && i % 5 == 0){
+//                System.out.println("FizzBuzz");
+//            } else if(i % 3 == 0){
+//                System.out.println("Fizz");
+//            }else if(i % 5 == 0){
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println(i);
+//            }
+//            i++;
+//        }
+//        System.out.print("Enter a integer: ");
+//        int userInteger = input.nextInt();
+//        String table = """
+//                Here is your table!
+//                number | squared | cubed
+//                -------|---------|------
+//                """;
+//        double i = 1;
+//        while(userInteger >= i){
+//            double squared = i * i;
+//            double cubed = i * i * i;
+//            table += String.format("%s     |%s     |%s\n",i, squared, cubed);
+//            i++;
+//        }
+//        System.out.printf("%s", table);
+        int loop = 0;
+        do {
+            System.out.println("Enter a numerical grade from 0 to 100: ");
+            String grade = input.nextLine();
+            int numberGrade = Integer.parseInt(grade);
+            if (numberGrade > 100 || numberGrade < 0){
+                continue;
             }
-            i++;
-        }
+            System.out.println("Your grade is: " + grade);
+            System.out.println("Continue?");
+            String response = input.nextLine();
+            if(response.equalsIgnoreCase("yes")){
+                System.out.print("Your grade is a: ");
+                if(numberGrade <= 59){
+                    System.out.println('F');
+                } else if(numberGrade <= 66){
+                    System.out.println('D');
+                } else if(numberGrade <= 79){
+                    System.out.println('C');
+                } else if(numberGrade <= 87){
+                    System.out.println('B');
+                } else if(numberGrade <= 98){
+                    System.out.println('A');
+                } else{
+                    System.out.println("A+");
+                }
+                loop += 1;
+            }
 
+        } while(loop == 0);
+//        System.out.printf()
+        input.close();
     }
 }
