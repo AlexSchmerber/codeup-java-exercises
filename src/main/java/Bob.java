2986fd768ed3ec51acc7a19b9291514f6b9df9ef
Alex Schmerber <alex.g.schmerber@gmail.com>
2023-02-09 11:11:12 -0600

    post StringExercise

---
+++ src/main/java/Bob.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Bob {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String continueBob;
+        String statementToBob;
+        String bobReply = "Bob replies: ";
+        do {
+            System.out.println("Say something to Bob.");
+            statementToBob = sc.nextLine();
+            if(statementToBob.endsWith("?")) {
+                System.out.println(bobReply + "Sure.");
+            } else if(statementToBob.endsWith("!")){
+                System.out.println(bobReply + "Woah. Chill out!");
+            } else if( statementToBob == null){
+                System.out.println(bobReply + "Fine. Be that way!");
+            } else{
+                System.out.println(bobReply + "Whatever.");
+            }
+            System.out.println("Continue talking to Bob? [y/n]");
+            continueBob = sc.nextLine();
+        } while ("y".equalsIgnoreCase(continueBob));
+        sc.close();
+    }
+}
---
+++ src/main/java/StringExercise.java
@@ -0,0 +1,10 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String matchedWords = "we don't need no ";
+        System.out.println(matchedWords + "education");
+        System.out.println(matchedWords + "thought control");
+        System.out.println("In windows, the main drive is usually C:\\\n");
+        System.out.println("I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!\n");
+    }
+}
