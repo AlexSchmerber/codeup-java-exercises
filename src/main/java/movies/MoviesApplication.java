e54691858a886924c7356a573a18cdda13a8d762
Alex Schmerber <alex.g.schmerber@gmail.com>
2023-02-14 16:00:26 -0600

    mid MoviesApplication

--- src/main/java/movies/MoviesApplication.java
+++ src/main/java/movies/MoviesApplication.java
@@ -2,14 +2,12 @@ package movies;
 import util.Input;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 
 import static movies.MoviesArray.findAll;
 
 public class MoviesApplication {
     private static Input userInput = new Input();
-
     public static void main(String[] args) {
         System.out.println("""
                 What would you like to do?
@@ -20,34 +18,36 @@ public class MoviesApplication {
                 3 - view movies in the drama category
                 4 - view movies in the horror category
                 5 - view movies in the scifi category""");
-//        int userResponse = userInput.getInt(0, 5);
+        int userResponse = userInput.getInt(0, 5);
         Movie [] movieArray = findAll();
         int increment = 0;
         System.out.println(Arrays.toString(movieArray));
 
-//        switch (userResponse) {
-//            case 1:
-//                System.out.println(Arrays.toString(movieArray));
-//                break;
-//            case 2:
-//                System.out.println("animated");
-//                for (Object movie: movieArray) {
-//
-//                }
-//                break;
-//            case 3:
-//
-//                System.out.println("drama");
-//                break;
-//            case 4:
-//
-//                System.out.println("horror");
-//                break;
-//            case 5:
-//
-//                System.out.println("sci-fi");
-//                break;
-//        }
+        switch (userResponse) {
+            case 1:
+                System.out.println(Arrays.toString(movieArray));
+                break;
+            case 2:
+                System.out.println("animated");
+                for (Movie movie: movieArray) {
+                    if("animated".equals(movie.getCategory())){
+                        System.out.println(movie);
+                    }
+                }
+                break;
+            case 3:
+
+                System.out.println("drama");
+                break;
+            case 4:
+
+                System.out.println("horror");
+                break;
+            case 5:
+
+                System.out.println("sci-fi");
+                break;
+        }
         userInput.sc.close();
     }
 }
