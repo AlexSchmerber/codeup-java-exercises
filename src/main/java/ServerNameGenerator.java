1b9428d9fd9acb84617cf32e04af0c9d823b2b3c
Alex Schmerber <alex.g.schmerber@gmail.com>
2023-02-14 14:03:42 -0600

    post ServerNameGenerator

--- src/main/java/ServerNameGenerator.java
+++ src/main/java/ServerNameGenerator.java
@@ -1,2 +1,13 @@
+import java.util.Random;
+
 public class ServerNameGenerator {
+    public static String[] adjectives = {"lively", "ostentatious", "lovely", "bodacious", "limp", "sour", "inquisitive", "sensative", "quacky", "tricky"};
+    public static String[] nouns = {"bird", "whale", "ostrich", "anteater", "mole", "lionfish", "squirrel", "jaguar", "donkey", "cow"};
+    public static String randomIndex (String[] array){
+        int rnd = new Random().nextInt(array.length);
+        return array[rnd];
+    }
+    public static void main(String[] args) {
+        System.out.println(randomIndex(adjectives) + "-" + randomIndex(nouns));
+    }
 }
