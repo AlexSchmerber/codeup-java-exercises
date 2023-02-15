479646b13a92daea0a30329d2b03eef3f04b375a
Alex Schmerber <alex.g.schmerber@gmail.com>
2023-02-15 12:38:52 -0600

    mid ShapesTest

--- src/main/java/shapes/Rectangle.java
+++ src/main/java/shapes/Rectangle.java
@@ -1,4 +1,19 @@
 package shapes;
 
 public class Rectangle {
+
+    public static int length;
+    public static int width;
+
+    public Rectangle(int inputLength, int inputWidth) {
+        length = inputLength;
+        width = inputWidth;
+    }
+
+    public int getArea(){
+        return length * width;
+    }
+    public int getPerimeter(){
+        return (length * 2) + (width * 2);
+    }
 }
